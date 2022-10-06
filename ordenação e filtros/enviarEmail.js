00ce150324da5f6f10d19665337e477a45bd912f
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-10-06 18:07:24 -0300

    Create enviarEmail.js

---
+++ ordenação e filtros/enviarEmail.js
@@ -0,0 +1,46 @@
+let computadorLigado = false
+let browseAberto = false
+let email
+let emailEnviado = false
+
+function ligarComputador() {
+    if(computadorLigado === false) {
+        computadorLigado = true
+    }
+
+    return computadorLigado
+}
+
+function abrirBrowse() {
+    if(browseAberto == false) {
+        browseAberto = true
+    }
+
+    return browseAberto
+}
+
+function clickEscrever(email) {
+    let corpoEmail = 'email corporativo para um diretor solicitando uma reunião de apresentação de indicadores.'
+    email = corpoEmail
+
+    return email
+}
+
+function clickEnviar() {
+    if(emailEnviado == false) {
+        emailEnviado = true
+    }
+
+    return emailEnviado
+}
+
+function enviarEmail() {
+    ligarComputador()
+    abrirBrowse()
+    clickEscrever()
+    clickEnviar()    
+
+    console.log('E-mail enviado com sucesso')
+}
+
+enviarEmail()
